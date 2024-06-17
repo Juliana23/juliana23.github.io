ccde817c55ec4dc5021238c56ba98af15a74972e
Juliana Amblard <juliana.amblard@gmail.com>
2024-06-17 09:54:26 +0100

    about

--- nav.js
+++ nav.js
@@ -1,18 +1,34 @@
 d3.select('#buttonLearn').on('click', function(d) {
 	d3.selectAll('.nav-link').classed('active', false);
-	console.log();
+	d3.selectAll('.section-content').style('display','none');
 	d3.select('#buttonLearn').classed('active', true);
 	d3.select('#learn').style('display','block');
-	d3.select('#home').style('display','none');
 });
 
 d3.select('#buttonHome').on('click', function(d) {
 	d3.selectAll('.nav-link').classed('active', false);
+	d3.selectAll('.section-content').style('display','none');
 	d3.select('#buttonHome').classed('active', true);
-	d3.select('#learn').style('display','none');
 	d3.select('#home').style('display','block');
 });
 
-d3.select('#print').on('click', function(d) {
-	window.print();
+d3.select('#buttonAbout').on('click', function(d) {
+	d3.selectAll('.nav-link').classed('active', false);
+	d3.selectAll('.section-content').style('display','none');
+	d3.select('#buttonAbout').classed('active', true);
+	d3.select('#about').style('display','block');
+});
+
+d3.select('#buttonWorkshop').on('click', function(d) {
+	d3.selectAll('.nav-link').classed('active', false);
+	d3.selectAll('.section-content').style('display','none');
+	d3.select('#buttonWorkshop').classed('active', true);
+	d3.select('#workshop').style('display','block');
+});
+
+d3.select('#buttonMeet').on('click', function(d) {
+	d3.selectAll('.nav-link').classed('active', false);
+	d3.selectAll('.section-content').style('display','none');
+	d3.select('#buttonMeet').classed('active', true);
+	d3.select('#meet').style('display','block');
 });
