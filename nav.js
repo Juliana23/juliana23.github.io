efe5b869bac2eb61c0e41accb1e2d378f03cfe13
Juliana Amblard <juliana.amblard@gmail.com>
2024-06-18 10:51:16 +0100

    navbar

--- nav.js
+++ nav.js
@@ -1,34 +1,34 @@
-d3.select('#buttonLearn').on('click', function(d) {
+d3.selectAll('.buttonLearn').on('click', function(d) {
 	d3.selectAll('.nav-link').classed('active', false);
 	d3.selectAll('.section-content').style('display','none');
-	d3.select('#buttonLearn').classed('active', true);
+	d3.select('#navLearn').classed('active', true);
 	d3.select('#learn').style('display','block');
 });
 
-d3.select('#buttonHome').on('click', function(d) {
+d3.selectAll('.buttonHome').on('click', function(d) {
 	d3.selectAll('.nav-link').classed('active', false);
 	d3.selectAll('.section-content').style('display','none');
-	d3.select('#buttonHome').classed('active', true);
+	d3.select('#navHome').classed('active', true);
 	d3.select('#home').style('display','block');
 });
 
-d3.select('#buttonAbout').on('click', function(d) {
+d3.selectAll('.buttonAbout').on('click', function(d) {
 	d3.selectAll('.nav-link').classed('active', false);
 	d3.selectAll('.section-content').style('display','none');
-	d3.select('#buttonAbout').classed('active', true);
+	d3.select('#navAbout').classed('active', true);
 	d3.select('#about').style('display','block');
 });
 
-d3.select('#buttonWorkshop').on('click', function(d) {
+d3.selectAll('.buttonWorkshop').on('click', function(d) {
 	d3.selectAll('.nav-link').classed('active', false);
 	d3.selectAll('.section-content').style('display','none');
-	d3.select('#buttonWorkshop').classed('active', true);
+	d3.select('#navWorkshop').classed('active', true);
 	d3.select('#workshop').style('display','block');
 });
 
-d3.select('#buttonMeet').on('click', function(d) {
+d3.selectAll('.buttonMeet').on('click', function(d) {
 	d3.selectAll('.nav-link').classed('active', false);
 	d3.selectAll('.section-content').style('display','none');
-	d3.select('#buttonMeet').classed('active', true);
+	d3.select('#navMeet').classed('active', true);
 	d3.select('#meet').style('display','block');
 });
